a8dfc11b1ac86e368b3f5812bf76ac4f52b65ea1
Mubanga-W-Chashi <wilbroadchashi@gmail.com>
2025-10-21 17:16:06 +0200

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,54 +1,3 @@
-# # # print('Hello world')
-
-
-# # def calc(a,b):
-# #     if 
-
-# #     elif:
-# #         def add(a,d):
-# #       return a + b 
-# #from usr_inp import a,b
-# def add(a,b):
-#     addition = a + b
-#     print('Value Sum: ' + str(addition))
-
-# def rmdr(a,b):
-#     remdr = a % b
-#     print('Value remainder: ' + str(remdr))
- 
-# def sub(a,b):
-#       diff = a - b
-#       print('Value diffence: ' + str(diff))
-
-# def mult(a,b):
-#       prdt = a - b
-#       print('Value product: ' + str(prdt))
-
-# def div(a,b):
-#       div_ans = a / b
-#       print('Value Division: ' + str(div_ans))
-
-# def pwr(a,b):
-#     pwr_prdt = a ** b
-#     print('Value Power product: ' + str(pwr_prdt))
-
-# def flr_div(a,b):
-#       f_div = a // b
-#       print('Value diffence: ' + str(f_div))
-
-
-
-
-# add(a,b)
-# sub(a,b)
-# div(a,b)
-# rmdr(a,b)
-# pwr(a,b)
-# flr_div(a,b) 
-# #add,sub,div,rmdr,pwr,flr_div
- # print(power(a,b))
-
-#Post learning return and Conditional statements
 
 def add(a,b):
     ans1 = a + b
@@ -84,14 +33,3 @@ def remdr(a,b):
     ans7 = a % b
     return ans7
 
-# print(add(3,5))
-
-# add,sub,div,multp,pwr,fl_div, remdr
-
-
-# if usr_operation == 'add':
-#     add(a,d)
-# elif usr_operation == 'sub':
-#     sub(a,b)
-
-# elif usr_
